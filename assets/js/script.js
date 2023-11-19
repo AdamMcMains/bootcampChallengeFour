27b1071451a01e5309caa189ddd4c3b0fd5483f2
Adam McMains <adammcmains@gmail.com>
2023-11-18 18:34:27 -0800

    started basic webpage structure and developed minor css properties onto it

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("questionOne").style.display = "none";
+
+document.getElementById("questionTwo").style.display = "none";
+
+document.getElementById("questionThree").style.display = "none";
+
+Object.onclick("startButton") = function(){
+    document.getElementById("startPage").style.display = "none";
+}
